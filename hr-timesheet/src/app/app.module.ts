06966e3b64c60ebd5b419d282912e31b95a91869
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2025-03-26 09:58:30 -0600

    I have added the navbar unit3

--- hr-timesheet/src/app/app.module.ts
+++ hr-timesheet/src/app/app.module.ts
@@ -4,18 +4,26 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './modules/app-routing.module';
+import { AppRoutingModule } from './modules/material/app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentsComponent } from './components/departments/departments.component';
 import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [AppComponent, DepartmentsComponent, TimesheetComponent, AnalyticsComponent, TopNavbarComponent, AnalyticsTableComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    DepartmentsComponent,
+    TimesheetComponent,
+    AnalyticsComponent,
+    TopNavbarComponent,
+    AnalyticsTableComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule, MatToolbarModule],
   providers: [provideClientHydration(withEventReplay())],
   bootstrap: [AppComponent],
 })
