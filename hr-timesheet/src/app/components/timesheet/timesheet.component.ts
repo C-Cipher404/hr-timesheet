fc328b76b89947062beb1e0eabbfc6a41c0496e5
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2025-03-27 10:12:08 -0600

    doing part 2 of unit8 template driven form adding deleting

--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -70,4 +70,19 @@ export class TimesheetComponent implements OnInit {
       return error;
     };
   }
+
+  getTotalHours(employee: Employee): number {
+    return (
+      employee.monday +
+      employee.tuesday +
+      employee.wednesday +
+      employee.thursday +
+      employee.friday +
+      employee.saturday +
+      employee.sunday
+    );
+  }
+  deleteEmployee(index: number): void {
+    this.employees.splice(index, 1);
+  }
 }
