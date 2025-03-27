7573acf6c7ff95fd045b04404ff0e83d16168018
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2025-03-27 09:55:50 -0600

    unit8 part one I updated the code but I still keep getting error

--- hr-timesheet/src/app/app.module.ts
+++ hr-timesheet/src/app/app.module.ts
@@ -1,30 +1,33 @@
 import { NgModule } from '@angular/core';
-import {
-  BrowserModule,
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './modules/material/app-routing.module';
 import { AppComponent } from './app.component';
 import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { DepartmentsComponent } from './components/departments/departments.component';
+import { MaterialModule } from './modules/material/material.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DepartmentsComponent,
     TimesheetComponent,
     AnalyticsComponent,
     TopNavbarComponent,
     AnalyticsTableComponent,
-    DepartmentsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, MatIconModule, MatToolbarModule],
-  providers: [provideClientHydration(withEventReplay())],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
