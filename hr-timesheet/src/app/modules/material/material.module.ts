7bc1045bf628ab01e596083d029c9b3a7aef35a3
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2025-03-31 10:06:40 -0600

    adding the part one unit9 uilding the table

--- hr-timesheet/src/app/modules/material/material.module.ts
+++ hr-timesheet/src/app/modules/material/material.module.ts
@@ -8,6 +8,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [],
@@ -21,6 +22,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
+    MatTabsModule,
   ],
 
   exports: [
@@ -32,6 +34,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
+    MatTabsModule,
   ],
 })
 export class MaterialModule {}
