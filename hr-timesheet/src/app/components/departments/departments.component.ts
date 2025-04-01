f2bb73d109ad8bbaa6f0606d39259474e5dec309
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2025-04-01 08:39:53 -0600

    geting unit 12 uploaded

--- hr-timesheet/src/app/components/departments/departments.component.ts
+++ hr-timesheet/src/app/components/departments/departments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from 'src/app/services/departments.service';
 import { Department } from '../../interfaces/department';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-departments',
@@ -10,15 +11,14 @@ import { Router } from '@angular/router';
 })
 export class DepartmentsComponent implements OnInit {
   departments: Department[] | undefined;
+  $departments$: Observable<Department[]> | undefined;
 
   constructor(
     private departmentsService: DepartmentsService,
     private router: Router
   ) {}
   ngOnInit(): void {
-    this.departmentsService.getDepartments().subscribe((departments) => {
-      this.departments = departments;
-    });
+    this.$departments$ = this.departmentsService.getDepartments();
   }
   goToDepartment(departmentId: string): void {
     this.router.navigate(['./timesheet', { id: departmentId }]);
